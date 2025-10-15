181a78f488e5431e53f12db9a7d2aafc61917d45
Leeka99 <dsrn0000@gmail.com>
2025-10-15 14:37:18 +0900

    feat: 일반 숫자 입력 처리 기능 구현
    1. 일반 숫자 입력 처리
    2. 쉼표(,) 또는 콜론(:)을 기준으로 분리
    3. 일반 숫자 입력에 대한 숫자의 합 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,10 +3,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         String input = Console.readLine();
         if (input.isBlank()) {
-            System.out.println(0);
+            System.out.println("공백입력");
+        }
+
+        // 첫번째 문자가 숫자일 경우
+        if (Character.isDigit(input.charAt(0))) {
+            char[] arr = input.toCharArray();
+            int answer = 0;
+            for (char token : arr) {
+                if (Character.isDigit(token)) {
+                    answer += Character.getNumericValue(token);
+                }
+                if (!Character.isDigit(token) && token != ',' && token != ':') {
+                    return;
+                }
+            }
+            System.out.println("합 :" + answer);
         }
     }
 }
