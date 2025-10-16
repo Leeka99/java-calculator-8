036f27e14493021e8aa40cebdc518a9e2199c16d
Leeka99 <dsrn0000@gmail.com>
2025-10-16 14:37:55 +0900

    refactor: 숫자 계산 로직 calculate 메서드화
    1. 숫자도 아니고 //로 시작하지 않는 경우 마지막 return 예외처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,13 +3,14 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Pattern;
 
 public class Application {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         String input = Console.readLine();
-        if (input.isBlank()) {
-            System.out.println("결과 : " + 0);
-            return;
-        }
+        System.out.println("결과 : " + calculate(input));
+    }
+
+    private static int calculate(String input) {
+        if (input.isBlank()) return 0;
 
         // 첫번째 문자가 숫자일 경우
         if (Character.isDigit(input.charAt(0))) {
@@ -29,7 +30,7 @@ public class Application {
                     throw new IllegalArgumentException();
                 }
             }
-            System.out.println("결과 : " + answer);
+            return answer;
         }
 
         // 첫번째 문자가 //로 시작하는 경우
@@ -37,10 +38,12 @@ public class Application {
             int answer = 0;
             char[] arr = input.toCharArray();
             String seperator = "";
-            if (input.matches("[^0-9]*")) throw new IllegalArgumentException(); // 문자만 입력된 경우
-            if (!input.contains("\\n")) throw new IllegalArgumentException(); // 잘못된 커스텀 구분자 사용
+            if (input.matches("[^0-9]*"))
+                throw new IllegalArgumentException(); // 문자만 입력된 경우
+            if (!input.contains("\\n"))
+                throw new IllegalArgumentException(); // 잘못된 커스텀 구분자 사용
             for (int i = 2; i < arr.length - 1; i++) {
-                if (arr[i] == '\\' && arr[i+1] == 'n') {
+                if (arr[i] == '\\' && arr[i + 1] == 'n') {
                     break;
                 }
                 seperator = seperator + arr[i];
@@ -50,16 +53,14 @@ public class Application {
             String[] numbers = sep.split(Pattern.quote(seperator));
             for (String number : numbers) {
                 int num = Integer.parseInt(number);
-                if (num < 0) throw new IllegalArgumentException();
+                if (num < 0)
+                    throw new IllegalArgumentException();
                 answer += num;
             }
-            System.out.println("결과 : " + answer);
+            return answer;
         }
 
-        // 숫자도 아니고 //로 시작하지도 않는다면 잘못된 커스텀 구분자 사용으로 판단.
-        if(!Character.isDigit(input.charAt(0)) && !input.startsWith("//")){
-            throw new IllegalArgumentException();
-        }
+        throw new IllegalArgumentException();
     }
 }
 
